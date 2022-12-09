487d88aa3350528e2e388d186e1473b0181c17e8
Nazavivian20 <nazafavourukwe18@gmail.com>
2022-12-08 17:11:09 -0800

    Piece of Art

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,19 +1,21 @@
-#include <stdio.o>
+#include <stdio.h>
 
 /**
- * maiin - Entry point 
  *
- * Return: Always 0 (Success) 
- */
+ *  * main - Entry Point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
 
 int main(void)
-{
-	printf("Size of a char: %lu byte($)\n", sizeof(char));
-	printf("Size of an int: %lu byte($)\n", sizeof(int));             
-        printf("Size of a long int: %lu byte($)\n", sizeof(long int));                                                              printf("Size of a long long: %lu byte($)\n", sizeof(long long));                                                            printf("Size of a float: %lu byte($)\n", sizeof(float));  
-        return (0)
 
+{
+
+		printf("with proper grammar, but the outcome is a piece of art,"\n);
+
+			return (0);
 
 }
-
-
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,26 @@
+#include <stdio.o>
+
+/**
+ *
+ *  * maiin - Entry point 
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success) 
+ *
+ *     */
+
+
+
+int main(void)
+
+{
+
+		printf("Size of a char: %lu byte($)\n", sizeof(char));
+		printf("Size of an int: %lu byte($)\n", sizeof(int));             
+                printf("Size of a long int: %lu byte($)\n", sizeof(long int));                                                              printf("Size of a long long: %lu byte($)\n", sizeof(long long));                                                            printf("Size of a float: %lu byte($)\n", sizeof(float));  
+
+				        return (0)
+
+}
+
