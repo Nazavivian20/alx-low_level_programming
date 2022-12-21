e0b7aa9829a665a97505269895631b5b5b167dca
Nazavivian20 <nazafavourukwe18@gmail.com>
2022-12-21 14:40:09 -0800

    Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
  * _strncpy - copy a string
- * @dest: input value
- * @src: input value
- * @n: input value
+ * @dest: copy to
+ * @src: copy from
+ * @n: number of char to be copied
  * Return: dest
  */
 char *_strncpy(char	*dest,	char	*src,	int	n)
